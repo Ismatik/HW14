dac2d70a3a6585d96b7e57cb9ca9c85f47434cf4
Ismatullo Mukhamedzhanov <ismatullo.mukhamedzhanov_2022@UCA.corp>
2021-09-10 18:24:33 +0500

    first commit

---
+++ HW14/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Barely have done previous, have no idea what to do. Need help with SQL hws.");
+        }
+    }
+}
